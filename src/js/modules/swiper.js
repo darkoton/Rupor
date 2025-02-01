9e6c4827534fdf5dd19dd0e5a9534f7fce9d895b
darkoton <darkoto12@gmail.com>
2025-02-01 21:19:33 +0200

    add start block

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,21 +1,33 @@
-new Swiper(".swiper", {
-  slidesPerView: 1,
-  spaceBetween: 15,
-  grabCursor: true,
-  loop: true,
-  speed: 800,
+document.addEventListener('DOMContentLoaded', createSliderStart);
+document.addEventListener('resize', createSliderStart);
+document.addEventListener('orientationchange ', createSliderStart);
 
-  autoplay: {
-    delay: 3500,
-  },
+function createSliderStart() {
+  console.log(1);
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints: {
-    767.8: {},
+  const width = window.innerWidth;
+  if (width < 768) {
+    new Swiper('.start__slider', {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      grabCursor: true,
+      loop: true,
+      speed: 800,
+      autoplay: {
+        delay: 3500,
+      },
+      navigation: {
+        nextEl: '.start__right',
+        prevEl: '.start__left',
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        500: {
+          slidesPerView: 2,
+        },
+      },
+    });
   }
-});
-
+}
